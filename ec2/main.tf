d05c5f279e79c5eaf4b1184dde18968b2166b013
yjs <yjs@presidio.com>
2026-05-05 11:34:57 +0530

    initial: terragrunt ec2 setup with count=1

---
+++ ec2/main.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "demo" {
+  count         = var.instance_count
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "atlantis-demo-${count.index + 1}"
+  }
+}
+
+output "instance_ids" {
+  value = aws_instance.demo[*].id
+}
