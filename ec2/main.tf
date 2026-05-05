7b136ec317e695cf98d6c2878cc6a95b6f1b746a
YOGESHnick <yogeshshamlin@gmail.com>
2026-05-05 13:08:21 +0530

    fix: add subnet_id for EC2 instances

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "demo" {
   count         = var.instance_count
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  subnet_id     = "subnet-058a17ee18465ecce"
 
   tags = {
     Name = "atlantis-demo-${count.index + 1}"
